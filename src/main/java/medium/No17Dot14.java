8a10be03e3adeb9da98ee638ae897f94e79ad345
HuangHuan <huanghuan@ifugle.com>
2021-09-03 14:49:51 +0800

    新做题

---
+++ src/main/java/medium/No17Dot14.java
@@ -0,0 +1,33 @@
+package medium;
+
+import java.util.Arrays;
+
+import org.junit.Test;
+
+/**
+ * @author huangh
+ * @version $
+ * @since 2021年09月03日 14:44:30
+ */
+public class No17Dot14 {
+
+    @Test
+    public void test() {
+        int[] arr = new int[] { 1, 3, 5, 7, 2, 4, 6, 8 };
+        int k = 4;
+        int[] ints = smallestK(arr, k);
+        System.out.println(ints);
+    }
+
+
+    public int[] smallestK(int[] arr, int k) {
+        Arrays.sort(arr);
+        int[] result = new int[k];
+        for (int i = 0; i < k; i++) {
+            result[i] = arr[i];
+        }
+        return result;
+
+    }
+
+}
